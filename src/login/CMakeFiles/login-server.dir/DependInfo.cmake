b0c1a713b459420a16365228314f73a3f4506bd1
Debian <debian@packer-output-383d7693-f272-4061-95f2-df4c24c1fb0f>
2022-03-12 21:14:18 +0000

    Webserver for guild icons added.

---
+++ src/login/CMakeFiles/login-server.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/debian/rathena/src/login/account.cpp" "src/login/CMakeFiles/login-server.dir/account.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/account.cpp.o.d"
+  "/home/debian/rathena/src/login/ipban.cpp" "src/login/CMakeFiles/login-server.dir/ipban.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/ipban.cpp.o.d"
+  "/home/debian/rathena/src/login/login.cpp" "src/login/CMakeFiles/login-server.dir/login.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/login.cpp.o.d"
+  "/home/debian/rathena/src/login/loginchrif.cpp" "src/login/CMakeFiles/login-server.dir/loginchrif.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/loginchrif.cpp.o.d"
+  "/home/debian/rathena/src/login/loginclif.cpp" "src/login/CMakeFiles/login-server.dir/loginclif.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/loginclif.cpp.o.d"
+  "/home/debian/rathena/src/login/logincnslif.cpp" "src/login/CMakeFiles/login-server.dir/logincnslif.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/logincnslif.cpp.o.d"
+  "/home/debian/rathena/src/login/loginlog.cpp" "src/login/CMakeFiles/login-server.dir/loginlog.cpp.o" "gcc" "src/login/CMakeFiles/login-server.dir/loginlog.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/debian/rathena/src/common/CMakeFiles/common.dir/DependInfo.cmake"
+  "/home/debian/rathena/src/common/CMakeFiles/common_base.dir/DependInfo.cmake"
+  "/home/debian/rathena/3rdparty/yaml-cpp/CMakeFiles/yaml-cpp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/login/CMakeFiles/login-server.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../../login-server"
+  "../../login-server.pdb"
+  "CMakeFiles/login-server.dir/account.cpp.o"
+  "CMakeFiles/login-server.dir/account.cpp.o.d"
+  "CMakeFiles/login-server.dir/ipban.cpp.o"
+  "CMakeFiles/login-server.dir/ipban.cpp.o.d"
+  "CMakeFiles/login-server.dir/login.cpp.o"
+  "CMakeFiles/login-server.dir/login.cpp.o.d"
+  "CMakeFiles/login-server.dir/loginchrif.cpp.o"
+  "CMakeFiles/login-server.dir/loginchrif.cpp.o.d"
+  "CMakeFiles/login-server.dir/loginclif.cpp.o"
+  "CMakeFiles/login-server.dir/loginclif.cpp.o.d"
+  "CMakeFiles/login-server.dir/logincnslif.cpp.o"
+  "CMakeFiles/login-server.dir/logincnslif.cpp.o.d"
+  "CMakeFiles/login-server.dir/loginlog.cpp.o"
+  "CMakeFiles/login-server.dir/loginlog.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/login-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
